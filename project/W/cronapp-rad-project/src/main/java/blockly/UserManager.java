0ddc75e4c7050ad0b9670ab36e276a265f4e1272
paulobalmer-cronapp <paulo.balmer@cronapp.io>
2021-11-30 22:56:22 -0300

    CRONAPP-3211 - É possível apagar o adm quando ele é o único administrador do sistema

--- project/W/cronapp-rad-project/src/main/java/blockly/UserManager.java
+++ project/W/cronapp-rad-project/src/main/java/blockly/UserManager.java
@@ -17,6 +17,48 @@ public static final int TIMEOUT = 300;
  * @param param_Entidade
  * @return Var
  */
+public static Var BeforeDelete(Var param_Entidade) throws Exception {
+    return new Callable<Var>() {
+
+        private Var authorizations = Var.VAR_NULL;
+        private Var administrators = Var.VAR_NULL;
+
+        public Var call() throws Exception {
+
+            authorizations =
+                    cronapi.database.Operations.query(Var.valueOf("app.entity.UserRole"),Var.valueOf("select u from UserRole u where u.user = :user AND u.role.normalizedName = :roleNormalizedName"),Var.valueOf("user",param_Entidade),Var.valueOf("roleNormalizedName",
+                            Var.valueOf("administrators")));
+
+            if (
+                    Var.valueOf(
+                            cronapi.list.Operations.size(authorizations).compareTo(
+                                    Var.valueOf(0)) > 0).getObjectAsBoolean()) {
+
+                administrators =
+                        cronapi.database.Operations.query(Var.valueOf("app.entity.UserRole"),Var.valueOf("select u.user from UserRole u where u.role.normalizedName = :roleNormalizedName"),Var.valueOf("roleNormalizedName",
+                                Var.valueOf("administrators")));
+
+                if (
+                        (
+                                Var.valueOf(
+                                        cronapi.list.Operations.size(administrators).compareTo(
+                                                Var.valueOf(1)) > 0)).negate().getObjectAsBoolean()) {
+
+                    cronapi.util.Operations.throwException(
+                            cronapi.util.Operations.createException(
+                                    cronapi.i18n.Operations.translate(Var.valueOf("OnlyOneAdministrator"))));
+                }
+            }
+            return Entidade;
+        }
+    }.call();
+}
+
+/**
+ *
+ * @param param_Entidade
+ * @return Var
+ */
 // UserManager
 public static Var BeforeInsert(Var param_Entidade) throws Exception {
  return new Callable<Var>() {
